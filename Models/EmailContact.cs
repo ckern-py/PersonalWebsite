bdc03761f2f163b37c49b47ff8107f4b9861edfc
Colin Kern <Colin.Kern@cunamutual.com>
2022-02-25 12:13:59 -0600

    Add a contact me page. Start GitHub page

---
+++ Models/EmailContact.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalWebsite.Models
+{
+    public class EmailContact
+    {
+        [BindProperty]
+        public string PersonEmail { get; set; }
+        [BindProperty]
+        public string EmailSubject { get; set; }
+        [BindProperty]
+        public string EmailMessage { get; set; }
+
+        public bool IsValid()
+        {
+            if (string.IsNullOrWhiteSpace(PersonEmail) || string.IsNullOrWhiteSpace(EmailSubject) || string.IsNullOrWhiteSpace(EmailMessage))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PersonalWebsite.Models;
+
+namespace PersonalWebsite.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string SubmitStatus { get; set; }
+
+        public void OnGet()
+        {
+            SubmitStatus = string.Empty;
+        }
+
+        [BindProperty]
+        public EmailContact ContactEmail { get; set; }
+
+        public void OnPost()
+        {
+            if (ContactEmail.IsValid())
+            {
+                //send the email or something  
+                SubmitStatus = "Email has been sent";
+            }
+            else
+            {
+                SubmitStatus = "Some information seems to be missing";
+            }
+        }
+    }
+}
