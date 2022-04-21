8cc2faf1fa67c74a5e37c49d70f08603d3b6e7f7
Colin Kern <Colin.Kern@cunamutual.com>
2022-04-21 12:22:18 -0500

    Call to stats API

--- Data/StatsAPI.cs
+++ Data/StatsAPI.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using PersonalWebsite.Domain;
+using PersonalWebsite.Models;
 using System;
 using System.Net.Http;
+using System.Text;
 
 namespace PersonalWebsite.Data
 {
@@ -18,12 +21,20 @@ namespace PersonalWebsite.Data
 
         public void RecordPageVisit(string pageName)
         {
+            StatsAPIBody reqBody = new StatsAPIBody()
+            {
+                PageName = pageName,
+                VisitDT = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
+                RequestingSystem = _configuration["REQUESTING_SYSTEM"]
+            };
+
             _httpClient.DefaultRequestHeaders.Clear();
 
-            _httpClient.DefaultRequestHeaders.Add("RequestingSystem", _configuration["REQUESTING_SYSTEM"]);
             _httpClient.DefaultRequestHeaders.Add("X-Environment-Header", _configuration["ENVIRONMENT_HEADER"]);
 
-            _httpClient.GetAsync(_configuration["STATS_API_URI"] + pageName);
+            HttpContent body = new StringContent(JsonConvert.SerializeObject(reqBody), Encoding.Default, "application/json");
+
+            _httpClient.PostAsync(_configuration["STATS_API_URI"], body);
         }
     }
 }
---
+++ Models/StatsAPIBody.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalWebsite.Models
+{
+    public class StatsAPIBody
+    {
+        public string RequestingSystem { get; set; }
+        public string PageName { get; set; }
+        public string VisitDT { get; set; }
+    }
+}
